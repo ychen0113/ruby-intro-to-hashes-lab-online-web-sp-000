6a385b13ba79b5d6ee26bdd624951c3129cf0f53
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 01:23:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,10 +51,6 @@ monopoly = {
 	      }
 	    },
 	    rent_in_dollars: {
-	      one_piece_owned: 25,
-	      two_pieces_owned: 50,
-	      three_pieces_owned: 100,
-	      four_pieces_owned: 200
 	    }
 	  }
 	}
