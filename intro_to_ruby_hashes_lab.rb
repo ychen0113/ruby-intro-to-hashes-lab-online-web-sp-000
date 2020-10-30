f18b3eb836a0f8a83b2b0028dc4675bf40a3dd4c
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 01:23:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,16 +42,12 @@ monopoly = {
 	    pieces: 4,
 	    names: {
 	      reading_railroad:{
-	        'mortgage_value' => '$100'
 	      },
 	      pennsylvania_railroad:{
-	        'mortgage_value' => '$200'
 	      },
 	      b_and_o_railroad:{
-	        'mortgage_value' => '$400'
 	      },
 	      shortline:{
-	        'mortgage_value' => '$800'
 	      }
 	    },
 	    rent_in_dollars: {
@@ -72,16 +68,12 @@ monopoly = {
 	    pieces: 4,
 	    names: {
 	      reading_railroad:{
-	        'mortgage_value' => '$100'
 	      },
 	      pennsylvania_railroad:{
-	        'mortgage_value' => '$200'
 	      },
 	      b_and_o_railroad:{
-	        'mortgage_value' => '$400'
 	      },
 	      shortline:{
-	        'mortgage_value' => '$800'
 	      }
 	    },
 	    rent_in_dollars: {
